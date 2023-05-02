a13206650a80b100293a842653b5cebd58f84d4d
Kevin Alfito <kevinalfito69@gmail.com>
2023-05-02 23:30:26 +0700

    Initial commit

---
+++ app/src/main/java/com/kevintekno/mysoundpool/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.kevintekno.mysoundpool
+
+import android.media.SoundPool
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var sp: SoundPool
+    private var soundId: Int = 0
+    private var spLoaded = false
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.btn_sound_pool)
+
+
+        sp = SoundPool.Builder()
+            .setMaxStreams(1)
+            .build()
+
+        sp.setOnLoadCompleteListener { _, _, status ->
+            if (status == 0) {
+                spLoaded = true
+            } else {
+                Toast.makeText(this@MainActivity, "Gagal load", Toast.LENGTH_SHORT).show()
+            }
+        }
+        soundId = sp.load(this, R.raw.game_retro, 1)
+
+        button.setOnClickListener {
+            if (spLoaded) {
+                sp.play(soundId, 1f, 1f, 0, 0, 1f)
+            }
+        }
+    }
+
+}
